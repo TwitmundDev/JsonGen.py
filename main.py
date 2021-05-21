100bc8a07e7926fa25c6cb35cdab9384403babeb
TwitmundDev <54693701+TwitmundDev@users.noreply.github.com>
2021-05-22 01:04:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,58 @@
+import tkMessageBox
 from Tkinter import *
 import os
+
+
+def MessageBoxInf():
+    label = "SIKE"
+    message="SIKE TOO "
+    top = Tk()
+    top.geometry("1x1")
+    tkMessageBox.showinfo("SIKE", "SIKE TOO ")
+
+    top.withdraw()
+
+
+
+def CreateDir(name_1,pdir_1,name_2,pdir_2):
+
+    # Path
+    path = os.path.join(pdir_1, name_1)
+
+    # Create the directory
+    # 'GeeksForGeeks' in
+    # '/home / User / Documents'
+    try:
+        os.mkdir(path)
+        print("Directory '% s' created" % name_1)
+    except WindowsError as e :
+        if e.winerror == 183:
+            print('Dossier deja existant ')
+
+    mode = 0o666
+
+    # Path
+    path = os.path.join(pdir_2, name_2)
+
+    try:
+        os.mkdir(path)
+        print("Directory '% s' created" % name_2)
+    except WindowsError as e :
+        if e.winerror == 183:
+            print('Dossier deja existant ')
+
+
+def CreateDir1_8_9():
+    return CreateDir(name_1="Test",name_2="Test2",pdir_1="C:\Users\twitm\Desktop",pdir_2="C:\Users\twitm\Desktop")
+
+
+def PrintDesktop():
+
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+    print("The Desktop path is: " + desktop)
+
+
 
 #Others app
 def App_1_8_9():
@@ -17,6 +70,13 @@ def App_1_8_9():
 
     label_title = Label(window_1_8_9, background=bg_colors, fg="white", text="1.8.9 module", font=("Courrier", 40))
     label_title.pack()
+
+    makedir= Button(window_1_8_9,
+                    text='Make dir',
+                    bg=bg_colors,
+                    font=('Courrier',25),
+                    command= MessageBoxInf
+                    ).pack()
 
 
     quit = Button(FrameQuit,text='Quit',fg="red",bg=bg_colors,font=('Courrier',15),command=window_1_8_9.destroy).pack()
@@ -88,4 +148,6 @@ def MainApp():
 if __name__ =='__main__':
     def Start():
         MainApp()
+
+
     Start()
